cffa818dddbbc4411ed6c3aca37e97d358f0502b
Elias <elias@floreteng.se>
2021-12-03 01:50:46 +0100

    Insertion sort, Pancake sort, randomize & styling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # eliasfl-sorting
+
+Visualization of sorting algorithms with d3.js
+
+Check it out: https://indaplus21.github.io/eliasfl-sorting/
--- index.js
+++ index.js
@@ -1,62 +1,198 @@
 // @ts-check
+/**
+ * Shuffle an array - randomize order of containing elements
+ * @param {any[]} array
+ */
+function shuffleArray(array) {
+  for (var i = array.length; i-- > 1; ) {
+    var j = Math.floor(Math.random() * i)
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
 
-// const data = [
-//   { index: 0, value: 4 },
-//   { index: 1, value: 10 },
-//   { index: 2, value: 3 },
-//   { index: 3, value: 1 },
-//   { index: 4, value: 9 },
-//   { index: 5, value: 5 },
-// ]
-
-let data = []
-for (let i = 0; i < 10; i++) {
-  data.push({
-    index: i,
-    // Random number between 1 and 10
-    value: Math.floor(Math.random() * 9) + 1,
-  })
+/**
+ * Min, max inclusive
+ * @param {number} length number of items
+ */
+function randomData(length) {
+  const min = 1
+  const max = 10
+  let data = []
+  for (let i = 0; i < length; i++) {
+    data.push({
+      id: i,
+      // Random number between min and max
+      value: Math.floor(Math.random() * (max - min)) + min,
+    })
+  }
+  shuffleArray(data)
+  return data
 }
 
 const svg = d3
-  .select(".viz")
-  .append("svg")
+  .select("#viz")
   .attr("viewBox", "0 0 100 100")
-  .attr("width", window.innerWidth)
-  .attr("height", window.innerHeight)
-  .attr("style", "width: 100%;")
+  .attr("width", 200)
+  .attr("height", 200)
   .attr("fill", "currentColor")
 
-const bar_height = 8
-const margin = 4
+let bars = parseInt(d3.select("#bars").attr("value"), 10)
+d3.select("#bars_val").text(bars)
+let speed = parseInt(d3.select("#speed").attr("value"), 10)
+d3.select("#speed_val").text(speed)
 
-const render = (
-  /** @type {Iterable<any> | d3.ValueFn<SVGSVGElement, any, any[] | Iterable<any>>} */
-  input_data
-) => {
-  const bars = svg.selectAll(".group").data(input_data, ({ index }) => index)
-  bars
-    .enter()
-    .append("rect")
-    .attr("class", "group")
-    .attr("x", 0)
-    .attr("y", ({ index }) => index * (bar_height + margin))
-    .attr("height", bar_height)
-    .transition()
-    .duration(2000)
-    .attr("width", ({ value }) => (value / 10) * 100)
+let bar_height = 100 / bars
+const gap = () => bar_height * 0.1
+bar_height = 100 / bars - gap()
+let selectedIndex = null
+
+/**
+ * @param {Iterable<any> | d3.ValueFn<d3.BaseType, any, any[] | Iterable<any>>} data
+ */
+const update = (data) => {
+  console.log("updating")
+  svg
+    .selectAll(".bar")
+    .data(data, ({ id }) => id)
+    .join(
+      (enter) =>
+        enter
+          .append("rect")
+          .attr("class", "bar")
+          .attr("x", 0)
+          .attr("y", (d, i) => i * (bar_height + gap()))
+          .attr("height", bar_height)
+          .transition()
+          .duration(2000)
+          .attr("width", ({ value }) => (value / 10) * 100),
+      (update) =>
+        update
+          .transition()
+          .duration(speed)
+          .attr("height", bar_height)
+          // https://tailwindcss.com/docs/customizing-colors#color-palette-reference colors.orange 500
+          .style("color", (d, i) => (i == selectedIndex ? "#F97316" : null))
+          .attr("width", ({ value }) => (value / 10) * 100)
+          .attr("y", (d, i) => i * (bar_height + gap()))
+    )
 }
-render(data)
 
-document.getElementsByClassName("viz")[0].addEventListener("click", () => {
-  console.log("Rerendering")
-  let data = []
-  for (let i = 0; i < 10; i++) {
-    data.push({
-      index: i,
-      // Random number between 1 and 10
-      value: Math.floor(Math.random() * 9) + 1,
-    })
+let data = randomData(bars)
+
+/**
+ * Swap two elements in array
+ * @param {Number} from
+ * @param {Number} to
+ */
+const swap = (from, to) => ([data[from], data[to]] = [data[to], data[from]])
+/**
+ * Use with await to sleep for selected ms
+ * @param {Number} ms
+ */
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+update(data)
+
+d3.select("#bars").on("input change", (event) => {
+  bars = event.target.value
+  d3.select("#bars_val").text(bars)
+  bar_height = 100 / bars - gap()
+  data = randomData(bars)
+  update(data)
+})
+
+d3.select("#speed").on("input change", (event) => {
+  speed = event.target.value
+  d3.select("#speed_val").text(speed)
+})
+
+d3.selectAll(".menubtn").on("click.all", () => {
+  d3.select("#summary").text("")
+})
+
+/** Randomize again */
+d3.select("#randomize").on("click", async function () {
+  d3.selectAll(".menubtn").attr("disabled", true)
+  data = randomData(bars)
+  update(data)
+  await sleep(speed)
+  d3.selectAll(".menubtn").attr("disabled", null)
+})
+
+d3.select("#stop").on("click", () => location.reload())
+
+/** Insertion sort */
+d3.select("#insertion").on("click", async function () {
+  d3.selectAll(".menubtn").attr("disabled", true)
+  let elapsedTime = 0
+  let steps = 0
+
+  let length = data.length
+  for (let i = 1; i < length; i++) {
+    let j = i
+    while (j > 0 && data[j].value > data[j - 1].value) {
+      selectedIndex = j
+      update(data)
+      await sleep(speed)
+
+      swap(j, j - 1)
+      j -= 1
+
+      selectedIndex = j
+      update(data)
+      await sleep(speed)
+      elapsedTime += speed
+      steps += 1
+    }
+    selectedIndex = null
+    update(data)
   }
-  render(data)
+
+  d3.select("#summary").text(
+    `Sorting took ${steps} steps (${Math.round(elapsedTime)} seconds)`
+  )
+  d3.selectAll("#randomize, #bars, #speed").attr("disabled", null)
+})
+
+/** Pancake sort */
+d3.select("#pancake").on("click", async () => {
+  d3.selectAll(".menubtn").attr("disabled", true)
+  let elapsedTime = 0
+  let steps = 0
+
+  const flip = (k) => {
+    let left = 0
+    while (left < k) {
+      swap(left, k)
+      k--
+      left++
+    }
+  }
+
+  const max_index = (k) =>
+    data.indexOf(data.slice(0, k).reduce((a, b) => (a.value > b.value ? a : b)))
+
+  let n = data.length
+  while (n > 1) {
+    let maxIdx = max_index(n)
+    if (maxIdx != n) {
+      flip(maxIdx)
+      update(data)
+      await sleep(speed)
+      elapsedTime += speed
+      steps += 1
+
+      flip(n - 1)
+      update(data)
+      await sleep(speed)
+      elapsedTime += speed
+      steps += 1
+    }
+    n--
+  }
+
+  d3.select("#summary").text(
+    `Sorting took ${steps} steps (${Math.round(elapsedTime)} seconds)`
+  )
+  d3.selectAll("#randomize, #bars, #speed").attr("disabled", null)
 })
